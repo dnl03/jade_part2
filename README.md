7020cb4c07395ccd053a28e21c36265f31e2f209
dnl03 <daniel.zeman08@gmail.com>
2024-04-19 23:13:24 +0200

    Z3-Done

---
+++ README.md
@@ -0,0 +1,4 @@
+Some sellers which were found at DF yellow pages may not respond to the CFP (PROPOSE lub REFUSE). Such situation is impossible in the current application. However, it is possible (and likely) in the open environment, where agents make autonomous decisions. The buyer's current procedure is not resistant to such possibility. Make it so.
+
+1. w sellerAgent należy zasymulować brak odpowiedzi od sellera.
+2. pomimo braku odpowiedzi od seller1/seller2 ksiązka powinna zostać kupiona
--- src/BookSellerAgent.java
+++ src/BookSellerAgent.java
@@ -12,107 +12,106 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 import java.util.*;
 
 public class BookSellerAgent extends Agent {
-  private Hashtable catalogue;
-  private BookSellerGui myGui;
+    private Hashtable catalogue;
+    private BookSellerGui myGui;
 
-  protected void setup() {
-    catalogue = new Hashtable();
-    myGui = new BookSellerGui(this);
-    myGui.display();
+    protected void setup() {
+        catalogue = new Hashtable();
+        myGui = new BookSellerGui(this);
+        myGui.display();
 
-    //book selling service registration at DF
-    DFAgentDescription dfd = new DFAgentDescription();
-    dfd.setName(getAID());
-    ServiceDescription sd = new ServiceDescription();
-    sd.setType("book-selling");
-    sd.setName("JADE-book-trading");
-    dfd.addServices(sd);
-    try {
-      DFService.register(this, dfd);
+        //book selling service registration at DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("book-selling");
+        sd.setName("JADE-book-trading");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        addBehaviour(new OfferRequestsServer());
+
+        addBehaviour(new PurchaseOrdersServer());
     }
-    catch (FIPAException fe) {
-      fe.printStackTrace();
+
+    protected void takeDown() {
+        //book selling service deregistration at DF
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        myGui.dispose();
+        System.out.println("Seller agent " + getAID().getName() + " terminated.");
     }
-    
-    addBehaviour(new OfferRequestsServer());
 
-    addBehaviour(new PurchaseOrdersServer());
-  }
-
-  protected void takeDown() {
-    //book selling service deregistration at DF
-    try {
-      DFService.deregister(this);
+    //invoked from GUI, when a new book is added to the catalogue
+    public void updateCatalogue(final String title, final int price) {
+        addBehaviour(new OneShotBehaviour() {
+            public void action() {
+                catalogue.put(title, new Integer(price));
+                System.out.println(getAID().getLocalName() + ": " + title + " put into the catalogue. Price = " + price);
+            }
+        });
     }
-    catch (FIPAException fe) {
-      fe.printStackTrace();
+
+    private class OfferRequestsServer extends CyclicBehaviour {
+        public void action() {
+            //proposals only template
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                // TODO: #4: Symulacja Braku Odpowiedzi od Seller 1
+                if (Objects.equals(getAID().getLocalName(), "seller1")) {
+                    System.out.println(getAID().getLocalName() + " Deciding not to respond to CFP from " + msg.getSender().getLocalName());
+                    return; // Nie wysyła żadnej odpowiedzi
+                }
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+                Integer price = (Integer) catalogue.get(title);
+                if (price != null) {
+                    //title found in the catalogue, respond with its price as a proposal
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(price.intValue()));
+                } else {
+                    //title not found in the catalogue
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
     }
-  	myGui.dispose();
-    System.out.println("Seller agent " + getAID().getName() + " terminated.");
-  }
 
-  //invoked from GUI, when a new book is added to the catalogue
-  public void updateCatalogue(final String title, final int price) {
-    addBehaviour(new OneShotBehaviour() {
-      public void action() {
-		catalogue.put(title, new Integer(price));
-		System.out.println(getAID().getLocalName() + ": " + title + " put into the catalogue. Price = " + price);
-      }
-    } );
-  }
-  
-	private class OfferRequestsServer extends CyclicBehaviour {
-	  public void action() {
-	    //proposals only template
-		MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
-		ACLMessage msg = myAgent.receive(mt);
-	    if (msg != null) {
-	      String title = msg.getContent();
-	      ACLMessage reply = msg.createReply();
-	      Integer price = (Integer) catalogue.get(title);
-	      if (price != null) {
-	        //title found in the catalogue, respond with its price as a proposal
-	        reply.setPerformative(ACLMessage.PROPOSE);
-	        reply.setContent(String.valueOf(price.intValue()));
-	      }
-	      else {
-	        //title not found in the catalogue
-	        reply.setPerformative(ACLMessage.REFUSE);
-	        reply.setContent("not-available");
-	      }
-	      myAgent.send(reply);
-	    }
-	    else {
-	      block();
-	    }
-	  }
-	}
 
-	
-	private class PurchaseOrdersServer extends CyclicBehaviour {
-	  public void action() {
-	    //purchase order as proposal acceptance only template
-		MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
-		ACLMessage msg = myAgent.receive(mt);
-	    if (msg != null) {
-	      String title = msg.getContent();
-	      ACLMessage reply = msg.createReply();
-	      Integer price = (Integer) catalogue.remove(title);
-	      if (price != null) {
-	        reply.setPerformative(ACLMessage.INFORM);
-	        System.out.println(getAID().getLocalName() + ": " + title + " sold to " + msg.getSender().getLocalName());
-	      }
-	      else {
-	        //title not found in the catalogue, sold to another agent in the meantime (after proposal submission)
-	        reply.setPerformative(ACLMessage.FAILURE);
-	        reply.setContent("not-available");
-	      }
-	      myAgent.send(reply);
-	    }
-	    else {
-		  block();
-		}
-	  }
-	}
+    private class PurchaseOrdersServer extends CyclicBehaviour {
+        public void action() {
+            //purchase order as proposal acceptance only template
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+                Integer price = (Integer) catalogue.remove(title);
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.INFORM);
+                    System.out.println(getAID().getLocalName() + ": " + title + " sold to " + msg.getSender().getLocalName());
+                } else {
+                    //title not found in the catalogue, sold to another agent in the meantime (after proposal submission)
+                    reply.setPerformative(ACLMessage.FAILURE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
 
 }
